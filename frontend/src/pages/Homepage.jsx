2d6a7cb60253fe5c78ff3f24a99662d0d5ab9351
PratyushSubhadarshi <119421621+PratyushSubhadarshi@users.noreply.github.com>
2024-06-27 00:24:50 +0530

    Update Homepage.jsx

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Layout } from "../components";
 const Homepage = () => {
-  return <Layout>This is Homepage version 1</Layout>;
+  return <Layout>This is Homepage version 2 vy prtyush</Layout>;
 };
 
 export default Homepage;
